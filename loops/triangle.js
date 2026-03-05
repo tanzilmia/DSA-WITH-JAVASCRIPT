ee3ee579d7a525b0a0ae59c7b06bb03c1047a56d
tanzilmia <ziltan590@gmail.com>
2026-03-05 11:57:13 +0600

    Comment out triangle pattern examples and related code

--- loops/triangle.js
+++ loops/triangle.js
@@ -1,11 +1,32 @@
 let n = 5;
 
-for(let i =n; i>0; i--){
+// for(let i =n; i>0; i--){
      
-    let row = '';
-    for(let j = 0; j<n; j++){
-        row += j + ' ';
-    }
-    console.log(row)
-}
- 
+//     let row = '';
+//     for(let j = 0; j<n; j++){
+//         row += j + ' ';
+//     }
+    // console.log(row)
+// }
+
+
+
+// for (let i = 0; i< n; i++){
+//     let row = '';
+//     for(let j = 0; j<= n-(i+1); j++){
+//         row += ' ';
+//     }
+//     for (let k =0; k<=i; k++){
+//         row += ' *';
+//     }
+//     console.log(row) 
+// }
+
+/*
+      *
+     * *
+    * * *
+   * * * *
+  * * * * *
+  
+*/
