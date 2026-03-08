3a1988ff336f20dcbc38091c29cd05aa6e78a7df
tanzilmia <ziltan590@gmail.com>
2026-03-08 08:48:46 +0600

    polindrome number checker

--- loops/palindrome.js
+++ loops/palindrome.js
@@ -11,3 +11,23 @@ const palindromeChecker = (x) =>{
 }
 
 palindromeChecker(123456)
+
+//  make this sortcurt
+
+const getPolindrome = (x) =>{
+    if(x < 0) return false;
+    let xCopy = x;
+    let reverse = 0;
+    while(x> 0){
+    const lastDigit = x % 10;
+    reverse = (10* reverse) + lastDigit;
+    x = Math.floor(x/10);
+    }
+    console.log(reverse)
+    return xCopy === reverse
+
+
+}
+
+const reuslt =  getPolindrome(1214);
+console.log(reuslt)
